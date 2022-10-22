bbc6a8d4ea14008c7ccb77f870a08b6c26adb416
Brandon Rodriguez <chilebr99@gmail.com>
2022-10-22 00:54:42 -0400

    Added player username errors

--- scripts/app.js
+++ scripts/app.js
@@ -1,5 +1,7 @@
 const playerConfigOverlay = document.getElementById("config-overlay");
 const backdrop = document.getElementById("backdrop");
+const formElement = document.querySelector("form")
+const errorsOutput = document.getElementById("config-errors")
 
 const editPlayer1Btn = document.getElementById("edit-player-1-btn");
 const editPlayer2Btn = document.getElementById("edit-player-2-btn");
@@ -9,3 +11,5 @@ editPlayer1Btn.addEventListener("click", openPlayerConfig);
 editPlayer2Btn.addEventListener("click", openPlayerConfig);
 cancelConfigBtn.addEventListener("click", closePlayerConfig);
 backdrop.addEventListener("click", closePlayerConfig);
+
+formElement.addEventListener("submit", savePlayerConfig)
--- scripts/config.js
+++ scripts/config.js
@@ -1,11 +1,24 @@
 function openPlayerConfig() {
-    playerConfigOverlay.style.display = "block"
-    backdrop.style.display = "block"
-
+  playerConfigOverlay.style.display = "block";
+  backdrop.style.display = "block";
 }
 
 function closePlayerConfig() {
-    playerConfigOverlay.style.display = "none"
-    backdrop.style.display = "none"
+  playerConfigOverlay.style.display = "none";
+  backdrop.style.display = "none";
+  formElement.firstElementChild.classList.remove("error");
+  errorsOutput.textContent = "";
+}
 
+function savePlayerConfig(event) {
+  event.preventDefault();
+  const formData = new FormData(event.target);
+  const enteredPlayername = formData.get("playername").trim()
+  
+  if (!enteredPlayername) {
+    event.target.firstElementChild.classList.add("error")
+    errorsOutput.textContent = "Enter a valid name!"
+    return;
+  }
+ 
 }
