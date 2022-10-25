21ffe5a72a16f769750f9de2c657ca67c287bd08
Brandon Rodriguez <chilebr99@gmail.com>
2022-10-24 20:34:01 -0400

    Added X, O turns

--- scripts/app.js
+++ scripts/app.js
@@ -1,4 +1,5 @@
 let editedPlayer = 0;
+let activePlayer = 0;
 
 const players = [
   {
@@ -17,10 +18,13 @@ const formElement = document.querySelector("form");
 const errorsOutput = document.getElementById("config-errors");
 const gameArea = document.getElementById("active-game")
 
+
 const editPlayer1Btn = document.getElementById("edit-player-1-btn");
 const editPlayer2Btn = document.getElementById("edit-player-2-btn");
 const cancelConfigBtn = document.getElementById("cancel-config-btn");
 const startNewGameBtn = document.getElementById("start-game");
+const gameFieldElements = document.querySelectorAll("#game-board li")
+console.log(gameFieldElements)
 
 editPlayer1Btn.addEventListener("click", openPlayerConfig);
 editPlayer2Btn.addEventListener("click", openPlayerConfig);
@@ -30,3 +34,7 @@ backdrop.addEventListener("click", closePlayerConfig);
 formElement.addEventListener("submit", savePlayerConfig);
 
 startNewGameBtn.addEventListener("click", startNewGame)
+
+for (const gameFieldElement of gameFieldElements) {
+gameFieldElement.addEventListener("click", selectGameField)
+}
