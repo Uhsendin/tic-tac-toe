612185875b1a7c5402046f712cba703b9be7a006
Brandon Rodriguez <chilebr99@gmail.com>
2022-10-27 20:55:47 -0400

    Added player tracking on grid squares

--- scripts/game.js
+++ scripts/game.js
@@ -13,11 +13,20 @@ function switchPlayer() {
     } else {
         activePlayer = 0
     }
-    activePlayerName.textContent =players[activePlayer].name;
+    activePlayerName.textContent = players[activePlayer].name;
 }
 
+
 function selectGameField(event) {
-event.target.textContent = players[activePlayer].symbol;
-event.target.classList.add("disabled")
+    const selectedField = event.target
+    const selectedColumn = selectedField.dataset.col -1;
+    const selectedRow = selectedField.dataset.row -1;
+
+    selectedField.textContent = players[activePlayer].symbol;
+    selectedField.classList.add("disabled")
+
+
+gameData[selectedRow][selectedColumn] = activePlayer + 1;
+console.log(gameData)
 switchPlayer()
 }
